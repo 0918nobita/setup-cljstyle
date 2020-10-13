80e043a6782a4b224a1211f732ae4b4bf1cc36c8
0918nobita <nobita.0918@gmail.com>
2020-10-13 16:20:34 +0900

    :art: Format code

--- src/path.ts
+++ src/path.ts
@@ -1,12 +1,18 @@
 import * as path from 'path';
+
 import { Branded, WithoutBrand } from './branded-types';
 
-type Path<T extends 'absolute' | 'relative'> = Branded<string, T>
+type Path<T extends 'absolute' | 'relative'> = Branded<string, T>;
 
-export const absolute =
-    <T extends string>(path: WithoutBrand<T>) => path as unknown as Path<'absolute'>;
-export const relative =
-    <T extends string>(path: WithoutBrand<T>) => path as unknown as Path<'relative'>;
+export const absolute = <T extends string>(
+    path: WithoutBrand<T>
+): Path<'absolute'> => (path as unknown) as Path<'absolute'>;
 
-export const joinPath = (base: Path<'absolute'>, relativePath: Path<'relative'>) =>
-    path.join(base, relativePath) as Path<'absolute'>;
+export const relative = <T extends string>(
+    path: WithoutBrand<T>
+): Path<'relative'> => (path as unknown) as Path<'relative'>;
+
+export const joinPath = (
+    base: Path<'absolute'>,
+    relativePath: Path<'relative'>
+): Path<'absolute'> => path.join(base, relativePath) as Path<'absolute'>;

a2cc6cf60a6b674b651dcea9e552fff73dbdd663
0918nobita <nobita.0918@gmail.com>
2020-10-13 16:19:30 +0900

    :wrench: Prettier, ESLint の設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es6: true, node: true },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    plugins: ['simple-import-sort'],
+    rules: {
+        'simple-import-sort/sort': 'error',
+    },
+    ignorePatterns: ['node_modules', 'dist'],
+    overrides: [
+        {
+            files: ['**/*.ts'],
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'prettier/@typescript-eslint',
+            ],
+            plugins: ['@typescript-eslint'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                sourceType: 'module',
+                project: './tsconfig.json',
+            },
+            rules: {
+                'no-undef': 'off',
+            },
+        },
+    ],
+};

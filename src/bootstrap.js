027d6a27e7414b09212bf54ddf30159768658b69
0918nobita <0918nobita@users.noreply.github.com>
2021-10-31 15:04:10 +0900

    :art: Simplify bootstrap.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
-var SetupCljstyle_Main_1 = require("../dce-output/SetupCljstyle.Main");
-(0, SetupCljstyle_Main_1.main)();
+require("../dce-output/SetupCljstyle.Main").main();

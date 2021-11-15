3fe4653b17fa27fd77e964aa5180e607bdcc179e
0918nobita <nobita.0918@gmail.com>
2021-11-15 14:52:53 +0900

    :sparkles: github-actions-toolkit に頼らずに input を取得するようにした

--- src/GitHub/Actions/Extension.purs
+++ src/GitHub/Actions/Extension.purs
@@ -2,17 +2,22 @@ module GitHub.Actions.Extension where
 
 import Prelude
 
-import Control.Monad.Except (ExceptT, mapExceptT, withExceptT)
+import Control.Monad.Except (ExceptT, mapExceptT, throwError)
 import Control.Monad.Reader (ReaderT, mapReaderT)
-import Data.Maybe (Maybe(Nothing))
+import Control.Monad.Trans.Class (lift)
+import Data.Maybe (Maybe(..))
+import Data.String (Pattern(..), Replacement(..), replaceAll, toUpper)
 import Effect.Aff (Aff)
 import GitHub.Actions.Core as Core
+import Node.Process (lookupEnv)
 import Types (EffectWithExcept, SingleError(..))
 
 inputExceptT :: String -> EffectWithExcept String
-inputExceptT name =
-  Core.getInput { name, options: Nothing }
-    # withExceptT \_ -> SingleError $ "Failed to get `" <> name <> "` input"
+inputExceptT name = do
+  valOpt <- lift $ lookupEnv $ "INPUT_" <> toUpper (replaceAll (Pattern " ") (Replacement "_") name)
+  case valOpt of
+    Just val -> pure val
+    Nothing -> throwError $ SingleError $ "Failed to get `" <> name <> "` input"
 
 group :: forall a. String -> Aff a -> Aff a
 group name aff = Core.group { name, fn: aff }

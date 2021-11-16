31a77719fed9d5d1323b2c1588b0695b76e9c174
0918nobita <nobita.0918@gmail.com>
2021-11-17 02:21:26 +0900

    :art: Improve readability

--- src/SetupCljstyle/Main.purs
+++ src/SetupCljstyle/Main.purs
@@ -27,17 +27,28 @@ import SetupCljstyle.Installer.Linux as Linux
 import SetupCljstyle.Installer.Win32 as Win32
 import SetupCljstyle.RawInputSource (class HasRawInputs, gatherRawInputs)
 import SetupCljstyle.RawInputSource.GitHubActions (ghaRawInputSource)
-import Types (AffWithExcept, SingleError(..))
+import Types (SingleError(..), AffWithExcept)
 
-type Env f i r = { fetcher :: f, installer :: i, rawInputSource :: r }
+type Env f i r =
+  { fetcher :: f
+  , installer :: i
+  , rawInputSource :: r
+  }
 
-mainReaderT :: forall f i r. Fetcher f => HasInstaller i => HasRawInputs r => ReaderT (Env f i r) AffWithExcept Unit
+mainReaderT
+  :: forall f i r
+   . Fetcher f
+  => HasInstaller i
+  => HasRawInputs r
+  => ReaderT (Env f i r) AffWithExcept Unit
 mainReaderT = do
   { installer, rawInputSource } <- ask
 
   rawInputs <- lift $ gatherRawInputs rawInputSource
 
-  { cljstyleVersion, runCheck } <- mapReaderT (group "Gather inputs") $ withReaderT (\r -> { fetcher: r.fetcher, rawInputs }) resolveInputs
+  { cljstyleVersion, runCheck } <-
+    mapReaderT (group "Gather inputs")
+      $ withReaderT (\r -> { fetcher: r.fetcher, rawInputs }) resolveInputs
 
   mapReaderT (group ("Install cljstyle " <> show cljstyleVersion)) do
     cachePath <- withReaderT (\_ -> cljstyleVersion) $ cache <|> runInstaller installer
@@ -47,26 +58,36 @@ mainReaderT = do
     RunCheck _ -> group "Run `cljstyle check`" $ lift $ execCmd "cljstyle" [ "check", "--verbose" ]
     DontRunCheck -> mempty
 
+mainWin32 :: AffWithExcept Unit
+mainWin32 = runReaderT mainReaderT
+  { fetcher: NodeFetcher
+  , installer: Win32.installer
+  , rawInputSource: ghaRawInputSource
+  }
+
+mainDarwin :: AffWithExcept Unit
+mainDarwin = runReaderT mainReaderT
+  { fetcher: NodeFetcher
+  , installer: Darwin.installer
+  , rawInputSource: ghaRawInputSource
+  }
+
+mainLinux :: AffWithExcept Unit
+mainLinux = runReaderT mainReaderT
+  { fetcher: NodeFetcher
+  , installer: Linux.installer
+  , rawInputSource: ghaRawInputSource
+  }
+
 main :: Effect Unit
 main =
   launchAff_ $ runExceptT $ catchError mainAff' handleError
   where
-  fetcher = NodeFetcher
-
-  rawInputSource = ghaRawInputSource
-
   mainAff' = case Process.platform of
-    Just Win32 ->
-      runReaderT mainReaderT { fetcher, installer: Win32.installer, rawInputSource }
-
-    Just Darwin ->
-      runReaderT mainReaderT { fetcher, installer: Darwin.installer, rawInputSource }
-
-    Just Linux ->
-      runReaderT mainReaderT { fetcher, installer: Linux.installer, rawInputSource }
-
+    Just Win32 -> mainWin32
+    Just Darwin -> mainDarwin
+    Just Linux -> mainLinux
     Just _ -> throwError $ SingleError "Unsupported platform"
-
     Nothing -> throwError $ SingleError "Failed to identify platform"
 
   handleError msg = liftEffect $ errorShow msg *> Process.exit 1

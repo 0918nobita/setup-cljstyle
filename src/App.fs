e97f6a7e16b695b4d31fed814ebbb2ffe856c5e9
0918nobita <nobita.0918@gmail.com>
2021-12-24 16:58:01 +0900

    :label: GitHub REST API に関する型定義を追加した

--- src/App.fs
+++ src/App.fs
@@ -1,14 +1,48 @@
 ﻿module App
 
+open Fable.Core
+
+type Path = Path of string
+
+type Version = Version of string
+
+[<Struct>]
+type FetchLatestReleaseArgs = {
+    authToken: string
+    owner: string
+    repo: string
+}
+
+type IGitHubRest =
+    abstract member FetchLatestRelease : FetchLatestReleaseArgs -> JS.Promise<Version>
+
+[<Sealed>]
+type GitHubRestTest private () =
+    static member Instance: IGitHubRest = GitHubRestTest()
+
+    interface IGitHubRest with
+        member _.FetchLatestRelease(_args: FetchLatestReleaseArgs) = Promise.lift (Version "0.15.0")
+
 type IGitHubActions =
-    abstract member AddPath : string -> unit
+    abstract member AddPath : Path -> unit
     abstract member GetInput : string -> string
     abstract member Group : ('a -> 'b) -> ('a -> 'b)
 
-type GitHubActionsTest =
+[<Sealed>]
+type GitHubActionsTest private () =
+    static member Instance: IGitHubActions = GitHubActionsTest()
+
     interface IGitHubActions with
-        member _.AddPath(_path: string) = ()
+        member _.AddPath(_path: Path) = ()
         member _.GetInput(_name: string) = "value"
         member _.Group(f: 'a -> 'b) = f
 
-printfn "Hello from F#"
+promise {
+    let! version = GitHubRestTest.Instance.FetchLatestRelease({
+        authToken = "token"
+        owner = "owner"
+        repo = "repo"
+    })
+    printfn "%A" version
+}
+|> ignore

450096ca2780189fcbb5fae4a06707052a4ed537
0918nobita <nobita.0918@gmail.com>
2021-12-24 16:44:27 +0900

    :label: IGitHubActions インターフェースを追加した

--- src/App.fs
+++ src/App.fs
@@ -1,3 +1,14 @@
 ﻿module App
 
+type IGitHubActions =
+    abstract member AddPath : string -> unit
+    abstract member GetInput : string -> string
+    abstract member Group : ('a -> 'b) -> ('a -> 'b)
+
+type GitHubActionsTest =
+    interface IGitHubActions with
+        member _.AddPath(_path: string) = ()
+        member _.GetInput(_name: string) = "value"
+        member _.Group(f: 'a -> 'b) = f
+
 printfn "Hello from F#"

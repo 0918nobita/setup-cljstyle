78e1b01a4852b4a6bfb23a4b5bffbbfe22171dde
0918nobita <nobita.0918@gmail.com>
2021-12-25 23:26:45 +0900

    :sparkles: Thoth.Json を用いたサンプルを追加した

--- src/App/App.fs
+++ src/App/App.fs
@@ -4,6 +4,20 @@ open GitHubActions.Impl
 open GitHubActions.Invoke
 open GitHubRest.Impl
 open GitHubRest.Invoke
+open Thoth.Json
+
+[<Struct>]
+type MyType =
+    | MyType of a : int * b : string
+
+    static member Decoder : Decoder<MyType> =
+        Decode.map2
+            (fun a b -> MyType (a, b))
+            (Decode.field "a" Decode.int)
+            (Decode.field "b" Decode.string)
+
+Decode.fromString MyType.Decoder """{ "a": 42, "b": "foo" }"""
+|> printfn "Result: %A"
 
 getInput GitHubActionsImpl.instance "cljstyle-version"
 |> printfn "Specified cljstyle version: %s"
